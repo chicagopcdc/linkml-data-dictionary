476ad6d23f2c62c398bdd5014b5eeb546b32c02a
Megan Moore <meganmoore@Megans-MacBook-Pro.local>
2023-05-03 13:08:50 -0500

    generated json data model, converted test validation data to json, need to test json to python model object instance and see how that works w bad data

---
+++ data_validation/validation_scripts/validate_json_w_python.py
@@ -0,0 +1,7 @@
+import json
+from python_model.data_dictionary_generated_fixed_quotes import Person
+
+
+# load 'data_validation/gen3_person.json' and convert to Person object
+
+# load 'data_validation/gen3_person_w_bad_data.json' and try to convert to Person object, see what errors occur
